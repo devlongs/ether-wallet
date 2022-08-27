0fdd6c5794ee10c13e155dbdf6117e3c694a2df6
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-27 10:43:04 +0000

    added deposit function, mapping and array

--- contracts/EtherWallet.sol
+++ contracts/EtherWallet.sol
@@ -6,12 +6,19 @@ import "hardhat/console.sol";
 
 contract EtherWallet {
     address payable public owner;
+    mapping(address => uint) public depositors;
+    address[] public depositorsAddresses;
 
     constructor() {
         owner = payable(msg.sender);
     }
 
     receive() external payable {}
+
+    function deposit() external payable {
+        depositors[msg.sender] += msg.value;
+        depositorsAddresses.push(msg.sender);
+    }
 
     function withdraw(uint _amount) public {
         require(msg.sender == owner, "You are not the owner");
